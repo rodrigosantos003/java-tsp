baf393657fdc484cdaae136988c7f0d79e661451
Rodrigo Santos <rodrigo_santos003@outlook.com>
2024-01-09 21:50:13 +0000

    Minor changes

--- bat_files/advanced.bat
+++ bat_files/advanced.bat
@@ -7,78 +7,97 @@ cd ../out/production/java-tsp
 REM Loop para executar o programa Java 10 vezes
 for /l %%i in (1,1,10) do (
     java Advanced ex4.txt 8 2 16 0.001 0.5
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java Advanced ex5.txt 8 3 25 0.001 0.5
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java Advanced ex6.txt 8 4 36 0.001 0.5
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java Advanced ex7.txt 8 5 49 0.001 0.5
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java Advanced ex8.txt 8 6 64 0.001 0.5
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java Advanced ex9.txt 8 7 81 0.001 0.5
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java Advanced ex10.txt 8 8 100 0.001 0.5
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java Advanced sp11.txt 8 9 121 0.001 0.5
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java Advanced uk12.txt 8 10 144 0.001 0.5
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java Advanced ex13.txt 8 20 169 0.001 0.5
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java Advanced burma14.txt 8 25 196 0.001 0.5
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java Advanced lau15.txt 8 30 225 0.001 0.5
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java Advanced ulysses16.txt 8 35 256 0.001 0.5
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java Advanced gr17.txt 8 40 289 0.001 0.5
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java Advanced ulysses22.txt 8 45 484 0.001 0.5
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java Advanced gr24.txt 8 50 576 0.001 0.5
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java Advanced fri26.txt 8 55 576 0.001 0.5
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java Advanced dantzig42.txt 8 60 1764 0.001 0.5
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java Advanced att48.txt 8 120 2304 0.001 0.5
+    echo.
 )
 
 echo Testes terminados!
--- bat_files/simulated_annealing.bat
+++ bat_files/simulated_annealing.bat
@@ -7,78 +7,97 @@ cd ../out/production/java-tsp
 REM Loop para executar o programa Java 10 vezes
 for /l %%i in (1,1,10) do (
     java SimulatedAnnealingTSP ex4.txt 8 3 100 0.009
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
-    java SimulatedAnnealingTSP ex5.txt 8 3 100 0.009
+    java SimulatedAnnealingTSP ex5.txt 8 3 100 0.9
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
-    java SimulatedAnnealingTSP ex6.txt 8 3 100 0.009
+    java SimulatedAnnealingTSP ex6.txt 8 3 100 0.9
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java SimulatedAnnealingTSP ex7.txt 8 3 100 0.99
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java SimulatedAnnealingTSP ex8.txt 8 3 100 0.99
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
-    java SimulatedAnnealingTSP ex9.txt 8 3 1000 0.9
+    java SimulatedAnnealingTSP ex9.txt 8 3 1000 0.99
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
-    java SimulatedAnnealingTSP ex10.txt 8 3 1000 0.9
+    java SimulatedAnnealingTSP ex10.txt 8 3 1000 0.99
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
-    java SimulatedAnnealingTSP sp11.txt 8 5 1000 0.99
+    java SimulatedAnnealingTSP sp11.txt 8 5 1000 0.999
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java SimulatedAnnealingTSP uk12.txt 8 5 1000 0.999
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java SimulatedAnnealingTSP ex13.txt 8 5 1000 0.999
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java SimulatedAnnealingTSP burma14.txt 8 5 1000 0.999
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java SimulatedAnnealingTSP lau15.txt 8 5 1000 0.999
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java SimulatedAnnealingTSP ulysses16.txt 8 5 100000 0.9999
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java SimulatedAnnealingTSP gr17.txt 8 5 100000 0.9999
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java SimulatedAnnealingTSP ulysses22.txt 8 10 1000000 0.99999
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java SimulatedAnnealingTSP gr24.txt 8 10 1000000 0.99999
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java SimulatedAnnealingTSP fri26.txt 8 10 1000000 0.99999
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java SimulatedAnnealingTSP dantzig42.txt 8 60 1000000 0.999999
+    echo.
 )
 
 for /l %%i in (1,1,10) do (
     java SimulatedAnnealingTSP att48.txt 8 120 100000 0.9999999
+    echo.
 )
 
 echo Testes terminados!
