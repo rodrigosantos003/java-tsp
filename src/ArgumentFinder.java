c831e9f22f09f2e625d3384d44e61608f333af64
Joao Fernandes <joaomlfernandes02@gmail.com>
2024-01-11 12:22:48 +0000

    argument tests

--- src/ArgumentFinder.java
+++ src/ArgumentFinder.java
@@ -18,75 +18,75 @@ class TestResult{
 public class ArgumentFinder {
     public static void main(String[] args) throws IOException, InterruptedException {
         // Definir valores iniciais para os argumentos
-        int[] initialTemperatures = {100, 1000, 1000, 10000, 100000};
-        double[] coolingRates = {0.1, 0.5, 0.9, 0.95, 0.99, 0.999, 0.9999, 0.99999};
-        String fileName = "att48.txt";
+        int[] initialTemperatures = {100, 1000, 1000, 10000, 100000, 500000};
+        double[] coolingRates = {0.1, 0.3, 0.5, 0.9, 0.95, 0.99, 0.999, 0.9999, 0.99999};
+        String[] files = {"ex4.txt", "ex7.txt", "ex8.txt", "ex9.txt", "ex10.txt", "sp11.txt", "uk12.txt", "ex13.txt", "burma14.txt", "lau15.txt", "ulysses16.txt", "gr17.txt", "ulysses22.txt", "gr24.txt", "fri26.txt", "dantzig42.txt", "att48.txt"};
         int threads = 8;
         int time = 1000;
 
         int testAmount = 10;
+
+        int totalTests = initialTemperatures.length * coolingRates.length * files.length;
+        int currentTest = 1;
 
         boolean executaTestes = true;
 
         List<TestResult> results = new LinkedList<>();
 
         if (executaTestes) {
-            for (int initialTemperature : initialTemperatures) {
-                for (double coolingRate : coolingRates) {
-                    for (int i = 0; i < testAmount; i++) {
-                        ProcessBuilder pb = new ProcessBuilder("java", "-cp", "out/production/java-tsp", "SimulatedAnnealingTSP", fileName, String.valueOf(threads), String.valueOf(time), String.valueOf(initialTemperature), String.valueOf(coolingRate));
-                        Process p = pb.start();
-                        p.waitFor();
+            for (String fileName : files) {
+                for (int initialTemperature : initialTemperatures) {
+                    for (double coolingRate : coolingRates) {
+                        for (int i = 0; i < testAmount; i++) {
+                            ProcessBuilder pb = new ProcessBuilder("java", "-cp", "out/production/java-tsp", "SimulatedAnnealingTSP", fileName, String.valueOf(threads), String.valueOf(time), String.valueOf(initialTemperature), String.valueOf(coolingRate));
+                            Process p = pb.start();
+                            p.waitFor();
+                        }
+
+                        TestResult result = apresentarDados(testAmount);
+
+                        results.add(result);
+
+                        System.out.println("Test " + currentTest + " / " + totalTests + " (" + fileName + ", " + initialTemperature + ", " + coolingRate + ") - " + result.distance + " | " + result.timeExec + " ms \n");
+                        currentTest++;
                     }
-
-                    TestResult result = apresentarDados(testAmount);
-
-                    results.add(result);
-
-                    System.out.println("\n");
                 }
-            }
-        }
 
-        //Apresenta os resultados por ordem do custo do melhor caminho e depois por ordem de tempo de execução
-        results.sort((o1, o2) -> {
-            if (o1.distance < o2.distance) {
-                return -1;
-            } else if (o1.distance > o2.distance) {
-                return 1;
-            } else {
-                if (o1.timeExec < o2.timeExec) {
+            //Apresenta os resultados por ordem do custo do melhor caminho e depois por ordem de tempo de execução
+            results.sort((o1, o2) -> {
+                if (o1.distance < o2.distance) {
                     return -1;
-                } else if (o1.timeExec > o2.timeExec) {
+                } else if (o1.distance > o2.distance) {
                     return 1;
                 } else {
-                    return 0;
+                    if (o1.timeExec < o2.timeExec) {
+                        return -1;
+                    } else if (o1.timeExec > o2.timeExec) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
                 }
+            });
+
+            //Escreve os resultados para um ficheiro de texto
+            BufferedWriter bw = new BufferedWriter(new FileWriter("test_results_" + fileName));
+            for (TestResult result : results) {
+                bw.write("Temperatura inicial: " + result.initialTemperature);
+                bw.newLine();
+                bw.write("Cooling rate: " + result.coolingRate);
+                bw.newLine();
+                bw.write("Custo medio do melhor caminho: " + result.distance);
+                bw.newLine();
+                bw.write("Tempo medio de execucao: " + result.timeExec + " ms");
+                bw.newLine();
+                bw.newLine();
             }
-        });
+            bw.close();
 
-        System.out.println("Resultados ordenados por custo do melhor caminho:\n");
-        for (TestResult result : results) {
-            System.out.print("Temperatura inicial: " + result.initialTemperature);
-            System.out.println("\t\tCooling rate: " + result.coolingRate);
-            System.out.print("Custo médio do melhor caminho: " + result.distance);
-            System.out.println("\tTempo médio de execução: " + result.timeExec + " ms");
+            results.clear();
         }
-
-        //Escreve os resultados para um ficheiro de texto
-        BufferedWriter bw = new BufferedWriter(new FileWriter("test_results_" + fileName));
-        for (TestResult result : results) {
-            bw.write("Temperatura inicial: " + result.initialTemperature);
-            bw.newLine();
-            bw.write("Cooling rate: " + result.coolingRate);
-            bw.newLine();
-            bw.write("Custo medio do melhor caminho: " + result.distance);
-            bw.newLine();
-            bw.write("Tempo medio de execucao: " + result.timeExec + " ms");
-            bw.newLine();
-            bw.newLine();
         }
-        bw.close();
     }
 
     static TestResult apresentarDados(int testAmount) throws IOException {
@@ -138,11 +138,6 @@ public class ArgumentFinder {
 
         double initialTemperature = Double.parseDouble(data.get(0)[4]);;
         double coolingRate = Double.parseDouble(data.get(0)[5]);
-
-        System.out.println("Temperatura inicial: " + initialTemperature);
-        System.out.println("Cooling rate: " + coolingRate);
-        System.out.println("Custo médio do melhor caminho: " + distance);
-        System.out.println("Tempo médio de execução: " + timeExec + " ms");
 
         return new TestResult(initialTemperature, coolingRate, distance, timeExec);
     }
